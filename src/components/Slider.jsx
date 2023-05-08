f058e04315b7f4655b200a91aa0f6f9bdc5eb01c
OllyLicoly <naina_kievna@mail.ru>
2023-05-08 10:47:28 +0700

    finalized project new file:   .gitignore new file:   .vscode/launch.json new file:   README.md new file:   package-lock.json new file:   package.json new file:   public/favicon.ico new file:   public/img/1.jpeg new file:   public/img/2.jpg new file:   public/img/3.jpg new file:   public/img/Tanay_001.jpg new file:   public/img/Tanay_002.jpg new file:   public/img/Tanay_003.jpg new file:   public/img/banner-new.jpeg new file:   public/img/first.jpg new file:   public/img/four.jpg new file:   public/img/icon.png new file:   public/img/logo1.png new file:   public/img/second.jpg new file:   public/img/three.jpg new file:   public/index.html new file:   public/logo192.png new file:   public/logo512.png new file:   public/manifest.json new file:   public/robots.txt new file:   src/App.css new file:   src/App.jsx new file:   src/App.test.js new file:   src/components/Description.jsx new file:   src/components/Footer.jsx new file:   src/components/Form.jsx new file:   src/components/Header.jsx new file:   src/components/Home.jsx new file:   src/components/Slider.jsx new file:   src/components/basket/Basket.jsx new file:   src/components/basket/ItemBasket.jsx new file:   src/components/cart/CartItem.jsx new file:   src/components/cart/Item.jsx new file:   src/components/favorites/Favorites.jsx new file:   src/components/favorites/Item.jsx new file:   src/index.css new file:   src/index.js new file:   src/logo.svg new file:   src/reportWebVitals.js new file:   src/setupTests.js

---
+++ src/components/Slider.jsx
@@ -0,0 +1,49 @@
+import { Button } from 'react-bootstrap';
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import {Link} from 'react-router-dom';
+
+const Slider = () => {
+  return (
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img width={'100%'} height={600}
+          className="d-block w-100"
+          src="./img/first.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Узнать больше</h3>
+          <p>Для получения подробной информации заполните форму обратной связи</p>
+          <Link exact to={'/form'}>
+            <Button variant="primary">Обратная связь</Button>
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img width={'100%'} height={600}
+          className="d-block w-100"
+          src="./img/second.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Невероятная природа</h3>
+          <p>Посетите самые красивые уголки планеты</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width={'100%'} height={600}
+          className="d-block w-100"
+          src="./img/three.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Головокружительные приключения</h3>
+          <p>Групповые и индивидуальные туры</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  )
+}
+
+export default Slider
